48e42889453df7a712770031578eff54f3a52dfe
Quarkubit <Abramovyd@gmail.com>
2025-04-21 23:25:13 +0300

    alpha(almost work)

--- src/selfgambler.c
+++ src/selfgambler.c
@@ -3,135 +3,132 @@
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 
-#define MAX_ROUNDS 10
-#define GUESS_SIGNAL SIGRTMIN
-#define START_SIGNAL (SIGRTMIN + 1)
+#define MAX_GAMES 10
 
-int N;
-pid_t other_pid;
-volatile sig_atomic_t secret;
+volatile sig_atomic_t targ;
 volatile sig_atomic_t guess;
-volatile sig_atomic_t round_ready = 0;
+volatile sig_atomic_t player;
+volatile sig_atomic_t finish;
+volatile sig_atomic_t attempts;
 
-// Обработчик для сигнала догадки
-void handle_guess(int sig, siginfo_t *info, void *context) {
-    (void)sig; (void)context;
-    guess = info->si_value.sival_int;
+void p1_handler(int sig, siginfo_t* info, void* context) {
+    (void)context;
+    if (sig == SIGUSR1) {
+        guess = info->si_value.sival_int;
+        if (guess == targ) {
+            kill(info->si_pid, SIGUSR1);
+            finish = 1;
+        } else {
+            kill(info->si_pid, SIGUSR2);
+        }
+        attempts++;
+    }
 }
 
-// Обработчик для стартового сигнала
-void handle_start(int sig) {
-    (void)sig;
-    round_ready = 1;
+void p2_handler(int sig, siginfo_t* info, void* context) {
+    (void)info; (void)context;
+    if (sig == SIGUSR1) {
+        finish = 1;
+    }
 }
 
-void host_mode() {
+void sending_guess(pid_t pid, int value) {
+    union sigval val;
+    val.sival_int = value;
+    sigqueue(pid, SIGUSR1, val);
+}
+
+void handler_setup(void (*handler)(int, siginfo_t*, void*), int sig) {
     struct sigaction sa;
-    sa.sa_sigaction = handle_guess;
+    sa.sa_sigaction = handler;
     sa.sa_flags = SA_SIGINFO;
     sigemptyset(&sa.sa_mask);
-    sigaction(GUESS_SIGNAL, &sa, NULL);
+    sigaddset(&sa.sa_mask, SIGUSR1);
+    sigaddset(&sa.sa_mask, SIGUSR2);
+    sigaction(sig, &sa, NULL);
+}
 
-    secret = rand() % N + 1;
-    printf("[HOST %d] Загадал: %d\n", getpid(), secret);
+void _game(int N, int cur_player) {
+    pid_t pid = getpid();
+    pid_t ppid = cur_player ? player : getppid();
 
-    // Отправляем стартовый сигнал и ждем подтверждения
-    kill(other_pid, START_SIGNAL);
+    if (cur_player) {
+        targ = rand() % N + 1;
+        printf("P1 (PID %d) загадал: %d\n", pid, targ);
 
-    sigset_t mask;
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGUSR1);
-    sigaddset(&mask, SIGUSR2);
-    sigprocmask(SIG_BLOCK, &mask, NULL);
+        finish = 0;
+        attempts = 0;
 
-    int attempts = 0;
-    while (1) {
-        pause(); // Ждем сигнал GUESS_SIGNAL
-        attempts++;
+        kill(ppid, SIGUSR1);
 
-        if (guess == secret) {
-            printf("[HOST] Угадано за %d попыток!\n", attempts);
-            kill(other_pid, SIGUSR1);
-            break;
-        } else {
-            kill(other_pid, SIGUSR2);
+        while(!finish) pause();
+
+        printf("Число %d угадано за %d попыток\n", targ, attempts);
+    } else {
+        int cur_guess;
+        finish = 0;
+
+        while(!finish) {
+            cur_guess = rand() % N + 1;
+            printf("P2 (PID %d) пробует: %d\n", pid, cur_guess);
+            sending_guess(ppid, cur_guess);
+            pause();
         }
     }
 }
 
-void guesser_mode() {
-    struct sigaction sa;
-    sa.sa_handler = handle_start;
-    sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
-    sigaction(START_SIGNAL, &sa, NULL);
-
-    // Ждем стартовый сигнал
-    while (!round_ready)
-        pause();
-    round_ready = 0;
-
-    sigset_t mask;
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGUSR1);
-    sigaddset(&mask, SIGUSR2);
-    sigprocmask(SIG_BLOCK, &mask, NULL);
-
-    int attempts = 0;
-    for (int g = 1; g <= N; ++g) {
-        union sigval val = {.sival_int = g};
-        sigqueue(other_pid, GUESS_SIGNAL, val);
-        attempts++;
-
-        int received_sig;
-        sigwait(&mask, &received_sig);
-
-        if (received_sig == SIGUSR1) {
-            printf("[GUESSER] Успех! Число %d найдено за %d попыток\n", g, attempts);
-            return;
-        }
+int set_N(int arg, char* argv[]) {
+    int N = 0;
+    if (arg <= 1) {
+        printf("Введите N: ");
+        scanf("%d", &N);
+    } else {
+        N = atoi(argv[1]);
+        printf("Используется N = %d\n", N);
     }
+    return N > 0 ? N : 100;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2 || (N = atoi(argv[1])) <= 0) {
-        fprintf(stderr, "Использование: %s <N>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+int main(int argc, char* argv[]) {
+    int N = set_N(argc, argv);
     srand(time(NULL));
 
-    // Блокируем сигналы для избежания гонок
     sigset_t block_mask;
     sigemptyset(&block_mask);
-    sigaddset(&block_mask, GUESS_SIGNAL);
-    sigaddset(&block_mask, START_SIGNAL);
     sigaddset(&block_mask, SIGUSR1);
     sigaddset(&block_mask, SIGUSR2);
     sigprocmask(SIG_BLOCK, &block_mask, NULL);
 
     pid_t pid = fork();
-    if (pid == -1) {
+    if (pid < 0) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
 
-    if (pid > 0) { // Родитель
-        other_pid = pid;
+    if (pid == 0) { // Дочерний процесс
+        handler_setup(p2_handler, SIGUSR1);
+        handler_setup(p2_handler, SIGUSR2);
         sigprocmask(SIG_UNBLOCK, &block_mask, NULL);
-        for (int i = 0; i < MAX_ROUNDS; i++) {
-            host_mode();
-            guesser_mode();
+
+        for (int i = 0; i < MAX_GAMES; i++) {
+            _game(N, 0);
         }
-        kill(pid, SIGTERM); // Завершаем дочерний процесс
-    } else { // Дочерний
-        other_pid = getppid();
+        exit(EXIT_SUCCESS);
+    } else { // Родительский процесс
+        player = pid;
+        handler_setup(p1_handler, SIGUSR1);
+        handler_setup(p1_handler, SIGUSR2);
         sigprocmask(SIG_UNBLOCK, &block_mask, NULL);
-        for (int i = 0; i < MAX_ROUNDS; i++) {
-            guesser_mode();
-            host_mode();
+
+        for (int i = 0; i < MAX_GAMES; i++) {
+            _game(N, 1);
         }
+
+        kill(pid, SIGTERM);
+        wait(NULL);
     }
 
     return 0;
